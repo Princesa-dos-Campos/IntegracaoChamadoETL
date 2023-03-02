76982c0f7ce815f97d3e8f13b45bd5f66b29ab63
MarianaLacerda013 <mariana.lacerda13@hotmail.com>
2023-03-02 14:58:56 -0300

    extracao
    extracao de dados criando o formulario para abertura de chamados

--- api/src/chamados/teste_extracao.py
+++ api/src/chamados/teste_extracao.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 #extrai dados do formulario por valor de coluna
 
 class TesteExtracao():
@@ -9,9 +11,29 @@ class TesteExtracao():
     def extracao(self):
         try:
             lista = []
-            for i in self.df['formulario']:
-                # print(i)
-                lista = i
-            # print(lista[1])
+            serie = self.df['formulario']
+            # print(type(serie))
+            for linha in serie:
+                # print("dados por linha: ",linha)
+                linha = linha.strip("']'")
+                linha = linha[2:]
+                linha = linha.split("', '")
+                empresa = linha[0]
+                fornecedor = linha[1]
+                codigo = linha[2]
+                agencia = linha[3]
+                razao = linha[4]
+                cnpj = linha[5]
+                ano = linha[6]
+                nf = linha[7]
+                data = linha[8]
+                natureza = linha[9]
+                email = linha[10]
+                custo = linha[11]
+                servico = linha [12]
+                lista.append([empresa, fornecedor, codigo, agencia, razao, cnpj, ano, nf, data, natureza, email, custo, servico])
+            formulario = pd.DataFrame(lista, columns=["empresa", "fornecedor", "codigo", "agencia_vendedor", "razao_social", "cnpj", "ano", "valor_nf", "data_pgmt", "natureza", "email",
+                                                      "custo", "servico"])
+            print(formulario)
         except Exception as e:
             print("Erro na extração do formulario...", e)
